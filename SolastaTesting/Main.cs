e028277156faa47123a3d060b5d5ee7445f9f08e
Philip Lee <github@pjlconsultants.co.uk>
2021-04-16 23:03:01 +0100

    Add project files.

---
+++ SolastaTesting/Main.cs
@@ -0,0 +1,135 @@
+﻿using HarmonyLib;
+using SolastaModApi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace SolastaTesting
+{
+    public class Main
+    {
+        [Conditional("DEBUG")]
+        internal static void Log(string msg) => Logger.Log(msg);
+
+        internal static void Error(Exception ex) => Logger?.Error(ex.ToString());
+        internal static void Error(string msg) => Logger?.Error(msg);
+        internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
+
+        internal static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                Logger = modEntry.Logger;
+
+                ModBeforeDBReady();
+
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        internal static class MainMenuScreen_RuntimeLoaded_Patch
+        {
+            internal static void Postfix(Runtime runtime)
+            {
+                ModAfterDBReady();
+            }
+        }
+
+        // ENTRY POINT IF YOU NEED SERVICE LOCATORS ACCESS
+        internal static void ModBeforeDBReady()
+        {
+            Log(nameof(ModBeforeDBReady));
+        }
+
+        // ENTRY POINT IF YOU NEED SAFE DATABASE ACCESS
+        internal static void ModAfterDBReady()
+        {
+            Log(nameof(ModAfterDBReady));
+
+            DumpAffinityDefinition(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityBludgeoningResistance);
+            DumpMonstersWithAffinity(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityBludgeoningResistance);
+            DumpAffinityDefinition(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityColdResistance);
+            DumpMonstersWithAffinity(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityColdResistance);
+            DumpAffinityDefinition(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityContagionFleshRotForce);
+            DumpMonstersWithAffinity(DatabaseHelper.FeatureDefinitionDamageAffinitys.DamageAffinityContagionFleshRotForce);
+
+            // TODO: create new ones / modify existing ones?
+            DatabaseRepository.GetDatabase<FeatureDefinitionDamageAffinity>().Add(new FeatureDefinitionDamageAffinityEx
+            {
+                // TODO: populate - unfortunately some/most fields are read only -- need to use reflection or Harmony to set them
+                // Name = "PsionicBlast"
+                // etc
+            });
+        }
+
+        /// <summary>
+        /// Show all monsters with specified affinity
+        /// </summary>
+        /// <param name="affinity"></param>
+        private static void DumpMonstersWithAffinity<T>(T affinity) where T : FeatureDefinitionAffinity
+        {
+            Log($"---- Monsters with affinity '{affinity.Name}'");
+
+            var monsters = DatabaseRepository
+                .GetDatabase<MonsterDefinition>()
+                .GetAllElements()
+                .Where(m => m.Features.OfType<T>().Where(f => !f.GuiPresentation.Hidden).Any(f => f == affinity));
+
+            foreach (var m in monsters.OrderBy(m => m.Name))
+            {
+                Log($"{m.Name}");
+            }
+        }
+
+        /// <summary>
+        /// Dump public properties of FeatureDefinitionDamageAffinity
+        /// </summary>
+        /// <param name="affinity"></param>
+        private static void DumpAffinityDefinition(FeatureDefinitionDamageAffinity affinity)
+        {
+            Log($"---- Affinity properties for '{affinity.Name}'");
+
+            foreach (var p in typeof(FeatureDefinitionDamageAffinity).GetProperties().OrderBy(p => p.Name))
+            {
+                Log($"{p.Name}: {p.GetValue(affinity, null)}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Possible custom affinity
+    /// </summary>
+    [HarmonyPatch(typeof(FeatureDefinitionDamageAffinity), "ModulateSustainedDamage")]
+    internal class FeatureDefinitionDamageAffinityEx : FeatureDefinitionDamageAffinity
+    {
+        // Additional properties
+        // ...
+
+        /// <summary>
+        /// Patch ModulateSustainedDamage
+        /// </summary>
+        internal static void Postfix(FeatureDefinitionDamageAffinity __instance, string damageType, int damage, List<string> sourceTags, ref int __result)
+        {
+            var fda = __instance as FeatureDefinitionDamageAffinityEx;
+
+            if (fda != null)
+            {
+                // original logic has run and damage calculated will be in __result
+
+                // custom logic here, modify __result 
+            }
+            // else do nothing (or something if required)
+        }
+    }
+}
---
+++ SolastaTesting/TestPatches.cs
@@ -0,0 +1,120 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace SolastaTesting
+{
+
+    //[HarmonyPatch(typeof(CharacterInspectionScreen), "Show")]
+    //internal static class CharacterInspectionScreen_Show
+    //{
+    //    public static void Prefix(CharacterInspectionScreen __instance, string filename, GuiScreen previousScreen)
+    //    {
+    //        RulesetEntity.DoNotAutoregister = true;
+
+    //        ServiceRepository.AddService(new DummyRulesetEntityService());
+    //    }
+
+    //    public static void Postfix(CharacterInspectionScreen __instance, string filename, GuiScreen previousScreen)
+    //    {
+    //        RulesetEntity.DoNotAutoregister = false;
+
+    //        ServiceRepository.RemoveService<DummyRulesetEntityService>();
+    //    }
+    //}
+
+    //[HarmonyPatch(typeof(RulesetEntity), "SerializeAttributes")]
+    internal static class RulesetEntity_SerializeAttributes
+    {
+        public static void Prefix(RulesetEntity __instance, IAttributesSerializer serializer,
+            IVersionProvider versionProvider)
+        {
+            string name;
+
+            try
+            {
+                name = __instance.Name;
+            }
+            catch (NullReferenceException)
+            {
+                name = "exception";
+            }
+
+            Main.Log($"RE_SA: Name={name}, Guid={__instance.Guid}, Att cnt={__instance.Attributes?.Count ?? 0}");
+        }
+    }
+
+    internal class DummyRulesetEntityService : IRulesetEntityService
+    {
+        public bool Dirty { get; set; }
+
+        public Dictionary<ulong, RulesetEntity> RulesetEntities => new Dictionary<ulong, RulesetEntity>();
+
+        public ulong GenerateGuid()
+        {
+            return 0UL;
+        }
+
+        public void RegisterEntity(RulesetEntity rulesetEntity)
+        {
+        }
+
+        public bool TryGetEntityByGuid(ulong guid, out RulesetEntity rulesetEntity)
+        {
+            rulesetEntity = null;
+            return false;
+        }
+
+        public void UnregisterEntity(RulesetEntity rulesetEntity)
+        {
+        }
+    }
+
+    //[HarmonyPatch(typeof(CharacterInspectionScreen), "HandleInput")]
+    internal static class CharacterInspectionScreen_HandleInput
+    {
+        public static bool Prefix(CharacterInspectionScreen __instance, InputCommands.Id command)
+        {
+            switch (command)
+            {
+                case InputCommands.Id.RotateCCW:
+                    SaveCharacter();
+                    break;
+            }
+
+            return true;
+
+            void SaveCharacter()
+            {
+                Main.Log("Save the character");
+
+                var heroCharacter = __instance.InspectedCharacter.RulesetCharacterHero;
+
+                //heroCharacter.ConditionsByCategory.Clear();
+
+                var name = heroCharacter.Name;
+                var builtin = heroCharacter.BuiltIn;
+                var guid = heroCharacter.Guid;
+                Main.Log($"Is built in={builtin}, guid={guid}.");
+
+                try
+                {
+                    heroCharacter.Name = "Exp" + name;
+                    heroCharacter.BuiltIn = false;
+
+                    AccessTools.Field(heroCharacter.GetType(), "guid").SetValue(heroCharacter, 0UL);
+
+                    ServiceRepository
+                        .GetService<ICharacterPoolService>()
+                        .SaveCharacter(heroCharacter, true);
+                }
+                finally
+                {
+                    heroCharacter.Name = name;
+                    heroCharacter.BuiltIn = builtin;
+                    AccessTools.Field(heroCharacter.GetType(), "guid").SetValue(heroCharacter, guid);
+                }
+            }
+        }
+    }
+}
